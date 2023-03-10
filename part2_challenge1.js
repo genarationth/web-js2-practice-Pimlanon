d074835ce75fa2d52e6a4b4b4060e39f4408b949
Pimlanon <125030381+Pimlanon@users.noreply.github.com>
2023-03-10 15:04:18 +0000

    complete task

--- part2_challenge1.js
+++ part2_challenge1.js
@@ -1,5 +1,5 @@
 const myBirthdayYear = 2000;
-let futureYear = 2026;
+let futureYear = 2029;
 
 let myAge = futureYear-myBirthdayYear;
-console.log(myAge);
+console.log('My age in 2029 is ' + myAge + 'years old.');
