35d8d2217452a2a46e05d538dd9d3526edb20f74
Pimlanon <125030381+Pimlanon@users.noreply.github.com>
2023-03-10 17:33:38 +0000

    add first sentence

---
+++ part2_challenge2.js
@@ -0,0 +1,7 @@
+let informationCard = {
+    userName: 'Folk',
+    place: 'Bangkok',
+    hobby: 'drawing picture'
+}
+//console.log(`Hi, my name is ${informationCard.userName}. I live in ${place} and enjoy ${hobby}`};
+console.log(`Hi, my name is ${informationCard.userName}`);
