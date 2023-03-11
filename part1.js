2600f95c028d3c8f3ed6829426ea1deb51a2df4a
Pimlanon <125030381+Pimlanon@users.noreply.github.com>
2023-03-11 00:21:45 +0000

    edit value

---
+++ part1.js
@@ -0,0 +1,10 @@
+//false
+//let first = false;
+//console.log(typeof first);
+//let hi = number(first);
+
+let hi = false;
+console.log(number(hi));
+
+console.log(Number(true));  
+console.log(Number(false)); 
--- part2_challenge1.js
+++ part2_challenge1.js
@@ -1,5 +1,5 @@
-const myBirthdayYear = 2000;
+const birthYear = 2000;
 let futureYear = 2029;
 
-let myAge = futureYear-myBirthdayYear;
+let myAge = futureYear-birthYear;
 console.log('My age in 2029 is ' + myAge + 'years old.');
