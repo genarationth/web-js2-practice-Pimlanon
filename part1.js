20cb12f769f11718a135d811312b68d8d6c2b4f7
Pimlanon <125030381+Pimlanon@users.noreply.github.com>
2023-03-11 01:00:54 +0000

    check true

---
+++ part1.js
@@ -0,0 +1,23 @@
+let a = false;
+let b = true;
+let c = 0;
+let d = 1;
+let e = "0";
+let f = "000";
+let g = "1";
+let h = NaN;
+let i = Infinity;
+let j = -Infinity;
+let k = "";
+let l = "20";
+let m = "Twenty";
+let n = null;
+let o = undefined;
+
+console.log(Number(a)); //0
+console.log(String(a)); //false
+console.log(Boolean(a)); //false
+
+console.log(Number(b)); //1
+console.log(String(b)); //true
+console.log(Boolean(b)); //true
